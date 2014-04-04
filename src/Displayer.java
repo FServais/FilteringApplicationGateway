ee0c9cd624220957be705885480da1b12cbcdd5f
Waliens <romainmormont@hotmail.com>
2014-04-04 17:55:39 +0200

    add displayer class

---
+++ src/Displayer.java
@@ -0,0 +1,71 @@
+package be.mormont.romain.icn.project1.server;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * A class for displaying message from server threads arriving in a queue.
+ * This class follows the singleton pattern.
+ * @author Romain
+ */
+public class Displayer extends Thread {
+	private LinkedBlockingQueue<String> lbq = null;
+	private boolean close = false;
+	
+	/** holds the single instance of the Displayer class */
+	private static Displayer singleton = null;
+	
+	/**
+	 * Constructs a Displayer
+	 */
+	private Displayer()
+	{
+		
+	}
+	
+	/**
+	 * Returns the unique instance of Displayer (singleton pattern). 
+	 * Before using the Displayer, the method setQueue() must be called to initialize the 
+	 * blocking queue.
+	 * @return the unique instance of the Displayer class
+	 */
+	public static synchronized Displayer getInstance()
+	{ 
+		if(singleton == null)
+			singleton = new Displayer();
+		
+		return singleton;
+	}
+	
+	/**
+	 * Initialize the queue of Displayer
+	 * @param lbq is the blocking queue in which the messages will come
+	 */
+	public synchronized void setQueue(LinkedBlockingQueue<String> lbq)
+	{
+		this.lbq = lbq;
+	}
+	
+	public void run()
+	{
+		if(lbq == null) // displayer queue hasn't been initialized
+			return;
+
+		while(!close || !lbq.isEmpty()) // output the messages as long as the thread has to run
+		{
+			try 
+			{
+				System.out.println(lbq.take());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Marks the thread as it has to be closed
+	 */
+	public void close()
+	{
+		close = true;
+	}
+}
