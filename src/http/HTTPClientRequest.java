8b3bfb6bfc45ae10e48efcfaf1308434f7fc0bc2
Waliens <romainmormont@hotmail.com>
2014-04-17 10:57:41 +0200

    ensure that HTTPClientConnection thread closes if an error occurs

--- src/http/HTTPClientRequest.java
+++ src/http/HTTPClientRequest.java
@@ -1,20 +1,13 @@
 package http;
 
 import java.net.Socket;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 import http.html.HTMLPage;
 import datastructures.Cache;
-import displayer.Displayer;
 
 /**
  * Class that handle a connection from a client that request a page.
@@ -43,27 +36,36 @@ public class HTTPClientRequest extends Thread {
 	{
 		System.out.println("New request");
 		// Wait for the request
-		String URL = new String(), request = new String();
+		StringBuilder sb = new StringBuilder();
+		String URL = null, request = null;
+		
 		try 
 		{
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+			BufferedReader br = new BufferedReader(isr);
 			
 			// Read the request
 			String line;
 			while((line = br.readLine()) != null && !line.equals(""))
 			{
-				request += (line + "\n");
+				sb.append(line + "\n");
 			}
 			
+			request = sb.toString();
 			//br.close();
 		} 
 		catch (IOException e) 
 		{
 			System.err.println("Error while getting request"); // Use Displayer instead...
+			return;
 		}
 		
 		if(request == null)
+		{
 			System.out.println("Null URL.");
+			return;
+		}
+		
 		else
 		{
 			DecodeClientRequest dcr = new DecodeClientRequest(request);
